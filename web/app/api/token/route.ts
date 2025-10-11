42954be98f764a523e1bd446f4a5309ffde70d0d
SanjayKumar <0508sanjaykumar@gmail.com>
2025-10-11 11:30:10 +0530

    feat: Added tokens for livekit

---
+++ web/app/api/token/route.ts
@@ -0,0 +1,33 @@
+import { AccessToken } from 'livekit-server-sdk';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const roomName = req.nextUrl.searchParams.get('room');
+  const username = req.nextUrl.searchParams.get('username');
+
+  if (!roomName || !username) {
+    return NextResponse.json(
+      { error: 'Missing room or username' },
+      { status: 400 }
+    );
+  }
+
+  const at = new AccessToken(
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!,
+    {
+      identity: username,
+    }
+  );
+
+  at.addGrant({
+    room: roomName,
+    roomJoin: true,
+    canPublish: true,
+    canSubscribe: true,
+  });
+
+  const token = await at.toJwt();
+
+  return NextResponse.json({ token });
+}
